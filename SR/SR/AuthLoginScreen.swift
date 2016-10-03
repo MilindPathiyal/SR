5f46a5ae956478414c41b4dc9d445ff846ed2907
MilindPathiyal <milindpathiyal@Milinds-MacBook-Air.local>
2016-10-03 13:36:07 -0700

    authsegue

--- SR/SR/AuthLoginScreen.swift
+++ SR/SR/AuthLoginScreen.swift
@@ -10,6 +10,47 @@ import UIKit
 
 class AuthLoginScreen: UIViewController {
     
+    @IBOutlet weak var usernameBox: UITextField!
+    @IBOutlet weak var passwordBox: UITextField!
+    @IBOutlet weak var Login: UIButton!
+    var go = true
+    
+    @IBAction func AuthLogin(sender: AnyObject) {
+        if (usernameBox.text == "lg") && (passwordBox.text == "lg") {
+            print("Auth Login")
+            
+            
+        }
+        else{
+            /*
+            let alert = UIAlertView()
+            alert.title = "Alert"
+            alert.message = "login/pass is wrong"
+            alert.addButtonWithTitle("Ok")
+            alert.show()
+            */
+            go = false
+            print("Auth Failed")
+            
+            
+        }
+        
+        
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool {
+        if let ident = identifier {
+            if ident == "login" {
+                if go != true {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
